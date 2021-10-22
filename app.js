4da28585b8654d50e40068ecf3bcf177d56a4d32
MS <ms@MSs-MacBook-Air.local>
2021-10-22 09:02:46 +0200

    Add gitignore, procfile and ports

--- app.js
+++ app.js
@@ -184,6 +184,12 @@ app.post("/submit", function(req,res){
    
 });
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000.")
+//heroku and local
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server has started successfully!")
 })
